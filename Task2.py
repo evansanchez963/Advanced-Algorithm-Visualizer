748a71af0dab174db8a07ed59ff23801988bb572
Frost274 <60369962+Frost274@users.noreply.github.com>
2020-02-26 13:37:41 -0800

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -20,18 +20,18 @@ def exp_DaC(a,n):
 def M_DB1(a,n):
     if n == 0:
         return 1
-    return exp_DB1(a, n-1) + 1
+    return M_DB1(a, n-1) + 1
 
 def M_DBC(a, n):
     if n == 0:
         return 1
     if(n%2 == 0):
-        return exp_DBC(a, n/2) + 1
-    return exp_DBC(a,(n-1)/2) + 2
+        return M_DBC(a, n/2) + 1
+    return M_DBC(a,(n-1)/2) + 2
 
 def M_DaC(a, n):
     if n == 0:
         return 1
     if(n%2 == 0):
-        return exp_DaC(a,n/2) + exp_DaC(a,n/2) + 1
-    return exp_DaC(a,(n-1)/2) + exp_DaC(a,(n-1)/2) + 2
+        return M_DaC(a,n/2) + M_DaC(a,n/2) + 1
+    return M_DaC(a,(n-1)/2) + M_DaC(a,(n-1)/2) + 2
