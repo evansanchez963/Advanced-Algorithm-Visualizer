a46dad67996c0b0db8a60f5137d22a5860dc5b95
Frost274 <60369962+Frost274@users.noreply.github.com>
2020-02-28 10:51:15 -0800

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,42 +1,31 @@
+count = 0
+
 def selectionSort(L,n):
+    global count
     for i in range(n-1):
         bigindex = 0
         for j in range(n-i):
+            count = count + 1
             if(L[j] > L[bigindex]):
                 bigindex = j
         L[j], L[bigindex] = L[bigindex], L[j]
     return L
 
 def insertionSort(L,n):
+    global count
     for i in range(1,n):
         index = L[i]
         j = i-1
         while(j >= 0 and index < L[j]):
+            count = count + 1
             L[j+1] = L[j]
             j = j - 1
+        count = count + 1
         L[j+1] = index
     return L
 
-def CselectionSort(L,n):
+def retcount():
+    global count
+    temp = count
     count = 0
-    for i in range(n-1):
-        bigindex = 0
-        for j in range(n-i):
-            if(L[j] > L[bigindex]):
-                bigindex = j
-            count = count + 1
-        L[j], L[bigindex] = L[bigindex], L[j]
-    return count
-
-def CinsertionSort(L,n):
-    count = 0
-    for i in range(1,n):
-        index = L[i]
-        j = i-1
-        while(j >= 0 and index < L[j]):
-            count = count + 1
-            L[j+1] = L[j]
-            j = j - 1
-        L[j+1] = index
-        count = count + 1
-    return count
+    return temp
