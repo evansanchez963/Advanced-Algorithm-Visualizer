44b88e49b9b4fb7140f9877144ac92b7bb835efc
Frost274 <60369962+Frost274@users.noreply.github.com>
2020-02-28 10:50:16 -0800

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,37 +1,34 @@
+count = 0
+
 def exp_DB1(a, n):
+    global count
     if n == 0:
         return 1
+    count = count + 1
     return a * exp_DB1(a, n-1)
 
 def exp_DBC(a, n):
+    global count
     if n == 0:
         return 1
     if(n%2 == 0):
+        count = count + 1
         return exp_DBC(a, n/2)**2
+    count = count + 2
     return a*exp_DBC(a,(n-1)/2)**2
 
 def exp_DaC(a,n):
+    global count
     if n == 0:
         return 1
     if(n%2 == 0):
+        count = count + 1
         return exp_DaC(a,n/2) * exp_DaC(a,n/2)
+    count = count + 2
     return a*exp_DaC(a,(n-1)/2)*exp_DaC(a,(n-1)/2)
 
-def M_DB1(a,n):
-    if n == 0:
-        return 1
-    return M_DB1(a, n-1) + 1
-
-def M_DBC(a, n):
-    if n == 0:
-        return 1
-    if(n%2 == 0):
-        return M_DBC(a, n/2) + 1
-    return M_DBC(a,(n-1)/2) + 2
-
-def M_DaC(a, n):
-    if n == 0:
-        return 1
-    if(n%2 == 0):
-        return M_DaC(a,n/2) + M_DaC(a,n/2) + 1
-    return M_DaC(a,(n-1)/2) + M_DaC(a,(n-1)/2) + 2
+def retcount():
+    global count
+    temp = count
+    count = 0
+    return temp
